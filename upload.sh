e91110f56388121dc48e188dea423a4b0893ad93
rxbi <manrousters@gmail.com>
2025-05-31 11:19:24 +0700

    initial commit

---
+++ upload.sh
@@ -0,0 +1,5 @@
+if [ -f ~/rom/out/target/product/RMX2185/*UNOFFICIAL*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/RMX2185/ && ls *UNOFFICIAL*.zip)"
+      rclone copy ~/rom/out/target/product/RMX2185/*UNOFFICIAL*.zip zona:zx -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/RMX2185/ && ls *UNOFFICIAL*.zip) Uploaded Successfully!"
+fi
